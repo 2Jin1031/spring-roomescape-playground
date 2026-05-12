d48088253ff2652a31df4e673b273377765d7802
2jin2.1031 <2jin2.1031@gmail.com>
2026-05-12 16:06:27 +0900

    feat: 2단계 - 예약 조회 기능 구현

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,13 +1,30 @@
 package roomescape;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import roomescape.domain.Reservation;
 
 @Controller
 public class RoomescapeController {
 
+    private final List<Reservation> reservations = new ArrayList<>();
+
     @GetMapping("/")
     public String getIndexPage() {
         return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String getReservationPage() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> getReservationPage2() {
+        return reservations;
     }
 }
