4156d678d95dbca4d23e71bf808005b313a7c293
2jin2.1031 <2jin2.1031@gmail.com>
2026-05-12 16:23:28 +0900

    feat: 3단계 - 예약 추가 / 취소 기능 구현

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,9 +1,15 @@
 package roomescape;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.domain.Reservation;
 
@@ -24,7 +30,29 @@ public class RoomescapeController {
 
     @GetMapping("/reservations")
     @ResponseBody
-    public List<Reservation> getReservationPage2() {
-        return reservations;
+    public ResponseEntity<List<Reservation>> getReservations() {
+        return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {
+        Reservation reservation1 = new Reservation((long) reservations.size() + 1, reservation.getName(),
+            reservation.getDate(), reservation.getTime());
+        reservations.add(reservation1);
+        return ResponseEntity
+            .created(URI.create("/reservations/" + reservations.size()))
+            .body(reservation1);
+    }
+
+    @DeleteMapping("/reservations/{reservationId}")
+    @ResponseBody
+    public ResponseEntity<Void> addReservation(@PathVariable Long reservationId) {
+        Reservation reservation = reservations.stream()
+            .filter(o -> o.getId() == reservationId)
+            .findFirst()
+            .get();
+        reservations.remove(reservation);
+        return ResponseEntity.noContent().build();
     }
 }
